7108243738556f7c76bec7be8f281b96f2712273
Luiz166 <otavio.tata.morais@gmail.com>
2024-10-04 14:52:36 -0300

    form for creating events

--- src/app/home/events/page.js
+++ src/app/home/events/page.js
@@ -48,13 +48,27 @@ export default function Events() {
 
             <div className="w-full flex flex-col items-center">
                 <div className="container">
-                    <h1 className="text-center font-semibold text-3xl">
+                    <h1 className="text-center font-semibold text-3xl dark:text-white">
                         Crie um evento!
                     </h1>
-                    <form>
-
+                    <form className="flex justify-center dark:text-white flex-col items-center space-y-3">
+                        <div className="space-x-2">
+                            <label htmlFor="title" className="">Titulo</label>
+                            <input type="text" name="title"/>
+                            <label htmlFor="desc" className="">Descrição</label>
+                            <input type="text" name="desc"/>
+                        </div>
+                        <div className="space-x-3">
+                            <input className="text-black" type="date"/>
+                            <input className="text-black" type="time"/>
+                            <label htmlFor="venue">Local</label>
+                            <select className="text-black" id="venues" name="venue">
+                                <option value="teste">teste</option>
+                            </select>
+                        </div>
+                        <button type="submit" className="border border-white/[0.3] px-5 py-1 rounded-lg hover:bg-white/[0.5] duration-300 ease-in-out">Criar</button>
                     </form>
-                    <h1 className="font-semibold text-3xl">
+                    <h1 className="font-semibold text-3xl dark:text-white">
                         Seus eventos
                     </h1>
                 </div>
