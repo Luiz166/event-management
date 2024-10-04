2b6b7cf35206cbcfc0bca15cf6506d8b347d264a
Luiz166 <otavio.tata.morais@gmail.com>
2024-10-04 16:57:01 -0300

    importing component homeNavbar

--- src/app/home/events/page.js
+++ src/app/home/events/page.js
@@ -1,10 +1,8 @@
 'use client';
-import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { Menu, MenuItem, HoveredLink } from "@/components/ui/NavbarMenu";
+import HomeNavbar from "@/components/HomeNavbar";
 
 export default function Events() {
-    const [active, setActive] = useState(null);
 
     const { data: session, status } = useSession()
     if (status === "loading") {
@@ -25,33 +23,13 @@ export default function Events() {
 
     return (
         <div className="min-w-[100vw] min-h-[100vh] bg-black flex items-center flex-col">
-            <Menu setActive={setActive}>
-                <MenuItem setActive={setActive} active={active} item={'Eventos'}>
-                    <div className="flex flex-col space-y-4 text-sm">
-                        <HoveredLink href="/home/events/">Seus eventos</HoveredLink>
-                        <HoveredLink href="/home/events/">Criar eventos</HoveredLink>
-                    </div>
-                </MenuItem>
-                <MenuItem setActive={setActive} active={active} item={'Locais'}>
-                    <div className="flex flex-col space-y-4 text-sm">
-                        <HoveredLink href="/home/venues/">Seus locais</HoveredLink>
-                        <HoveredLink href="/home/venues/">Criar locais</HoveredLink>
-                    </div>
-                </MenuItem>
-                <MenuItem setActive={setActive} active={active} item={'Perfil'}>
-                    <div className="flex flex-col space-y-4 text-sm">
-                        <HoveredLink href="/home/">Configurações</HoveredLink>
-                        <HoveredLink href="/home/signout/">Sair</HoveredLink>
-                    </div>
-                </MenuItem>
-            </Menu>
-
+            <HomeNavbar/>
             <div className="w-full flex flex-col items-center">
                 <div className="container">
                     <h1 className="text-center font-semibold text-3xl dark:text-white">
                         Crie um evento!
                     </h1>
-                    <form className="flex justify-center dark:text-white flex-col items-center space-y-3">
+                    <form className="flex mt-5 justify-center dark:text-white flex-col items-center space-y-3">
                         <div className="space-x-2">
                             <label htmlFor="title" className="">Titulo</label>
                             <input type="text" name="title"/>
