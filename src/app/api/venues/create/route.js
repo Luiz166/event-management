3bfc444c5520b5d6632d6149b2f13c311964655f
Luiz166 <otavio.tata.morais@gmail.com>
2024-10-04 18:21:55 -0300

    fixed typo

--- src/app/api/venues/create/route.js
+++ src/app/api/venues/create/route.js
@@ -2,7 +2,7 @@ import prisma from "@/lib/db";
 
 export async function POST(req){
     try{
-        const { name, address, capacity, user_id } = await req.Json();
+        const { name, address, capacity, user_id } = await req.json();
 
         const venue = await prisma.venues.create({
             data: {
@@ -15,6 +15,6 @@ export async function POST(req){
 
          return new Response(JSON.stringify({ message: 'Local criado com sucesso: ', venue }), { status: 201 })
     }catch (err){
-        return new Response(JSON.stringify({ message: 'Erro ao criar locar:', err }), { status: 500 })
+        return new Response(JSON.stringify({ message: 'Erro ao criar local:', err }), { status: 500 })
     }
 }
