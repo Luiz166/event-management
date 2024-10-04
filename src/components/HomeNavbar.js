1f80581b595a95bef0f7b667854e6df16a811936
Luiz166 <otavio.tata.morais@gmail.com>
2024-10-04 16:56:39 -0300

    creating a component for the home navbar for better reutilization

---
+++ src/components/HomeNavbar.js
@@ -0,0 +1,28 @@
+import { Menu, MenuItem, HoveredLink } from "@/components/ui/NavbarMenu";
+import { useState } from "react";
+
+export default function HomeNavbar(){
+    const [active, setActive] = useState(null);
+    return(
+        <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item={'Eventos'}>
+            <div className="flex flex-col space-y-4 text-sm">
+                <HoveredLink href="/home/events/">Seus eventos</HoveredLink>
+                <HoveredLink href="/home/events/">Criar eventos</HoveredLink>
+            </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item={'Locais'}>
+            <div className="flex flex-col space-y-4 text-sm">
+                <HoveredLink href="/home/venues/">Seus locais</HoveredLink>
+                <HoveredLink href="/home/venues/">Criar locais</HoveredLink>
+            </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item={'Perfil'}>
+            <div className="flex flex-col space-y-4 text-sm">
+                <HoveredLink href="/home/">Configurações</HoveredLink>
+                <HoveredLink href="/home/signout">Sair</HoveredLink>
+            </div>
+        </MenuItem>
+        </Menu>
+    )
+}
