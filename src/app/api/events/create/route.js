9469c15e7fe34e310b47f60957accb8c5aef66a4
Luiz166 <otavio.tata.morais@gmail.com>
2024-10-08 13:01:30 -0300

    removing column time from POST query

--- src/app/api/events/create/route.js
+++ src/app/api/events/create/route.js
@@ -3,20 +3,20 @@ import prisma from "@/lib/db";
 export async function POST(req){
     try{
 
-        const { title, description, date, time, venue_id, user_id } = await req.json();
+        const { title, description, date, venue_id, user_id } = await req.json();
         
         const event = await prisma.events.create({
             data: {
                 title,
                 description,
                 date,
-                time,
                 venue_id,
                 user_id,
             }
         })
         return new Response(JSON.stringify({ message: "Evento criado com sucesso", event }), { status: 201 });
     }catch(err){
+        console.log(err)
         return new Response(JSON.stringify({ message: "Erro ao criar evento: ", err }), { status: 500 })
     }
 }
