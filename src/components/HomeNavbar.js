3e5cbd96fcf7609130c7c0369f5965ff24e1d861
Luiz166 <otavio.tata.morais@gmail.com>
2024-10-08 14:57:33 -0300

    adding link to /home

--- src/components/HomeNavbar.js
+++ src/components/HomeNavbar.js
@@ -5,6 +5,11 @@ export default function HomeNavbar(){
     const [active, setActive] = useState(null);
     return(
         <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item={'Home'}>
+            <div className="flex flex-col space-y-4 text-sm">
+                <HoveredLink href="/home/">Home</HoveredLink>
+            </div>
+        </MenuItem>
         <MenuItem setActive={setActive} active={active} item={'Eventos'}>
             <div className="flex flex-col space-y-4 text-sm">
                 <HoveredLink href="/home/events/">Seus eventos</HoveredLink>
